e6502b4fa39d39f6566761d37fe0f7279828e917
shine <xiaoshaoqq@gmail.com>
2019-06-28 14:33:41 +0800

    详情

--- src/pages/book_detail/index.ts
+++ src/pages/book_detail/index.ts
@@ -3,9 +3,26 @@ import Request from '../../utils/request'
 Page({
   data: {
     detail: {},
-    id: undefined
+    id: undefined,
+    catalog: ''
   },
-
+  /**
+   * 把目录字符串转化为数组
+   * @param content 目录字符串
+   */
+  transferStringToArray(content: string) {
+    let string = content
+    try {
+      string = string.replace(/\r\n/g, '<br>')
+      string = string.replace(/\n/g, '<br>')
+    } catch (e) {
+      alert(e.message)
+    }
+    this.setData!({
+      catalog: string.split('<br>')
+    })
+    return
+  },
   /**
    * 获取图书的详情
    * @param  {number} id 图书id
@@ -16,6 +33,7 @@ Page({
       this.setData!({
         detail: res.data
       })
+      this.transferStringToArray(res.data.catalog)
     })
   },
   onLoad(options: any) {
