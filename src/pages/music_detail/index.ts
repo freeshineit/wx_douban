a8f9c27f370b4bfea25fb279dcf5902c037abc7f
shine <xiaoshaoqq@gmail.com>
2019-06-29 13:01:53 +0800

    字段

--- src/pages/music_detail/index.ts
+++ src/pages/music_detail/index.ts
@@ -7,23 +7,6 @@ Page({
     catalog: ''
   },
   /**
-   * 把目录字符串转化为数组
-   * @param content 目录字符串
-   */
-  //   transferStringToArray(content: string) {
-  //     let string = content
-  //     try {
-  //       string = string.replace(/\r\n/g, '<br>')
-  //       string = string.replace(/\n/g, '<br>')
-  //     } catch (e) {
-  //       alert(e.message)
-  //     }
-  //     this.setData!({
-  //       catalog: string.split('<br>')
-  //     })
-  //     return
-  //   },
-  /**
    * 获取图书的详情
    * @param  {number} id 图书id
    *
@@ -33,7 +16,6 @@ Page({
       this.setData!({
         detail: res.data
       })
-      //   this.transferStringToArray(res.data.catalog)
     })
   },
   onLoad(options: any) {
