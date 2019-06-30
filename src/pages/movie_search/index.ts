76c468a2bc51b7ee478ce11247bb180c41731b99
shine <xiaoshaoqq@gmail.com>
2019-06-30 23:43:09 +0800

    搜索接口不能用了 夭折了 我也很无奈

--- src/pages/movie_search/index.ts
+++ src/pages/movie_search/index.ts
@@ -1,25 +1,35 @@
 //index.js
 //获取应用实例
 
-// import Request from '../../utils/request'
+import Request from '../../utils/request'
 
 Page({
   data: {
-    detail: [],
+    list: [],
     total: 0,
-    title: ''
+    query: {
+      q: '张艺谋'
+      //   tag: undefined,
+      //   start: 0,
+      //   count: 20
+    }
   },
 
-  getIndexData() {
-    // Request.get('/v2/movie/in_theaters').then((res: any) => {
-    //   this.setData!({
-    //     theaters: res.data.subjects,
-    //     title: res.data.title,
-    //     total: res.data.total
-    //   })
-    // })
+  getMovieList() {
+    const { query } = this.data
+    Request.get('/v2/movie/search', query).then((res: any) => {
+      console.log(res)
+      //   this.setData!({
+      //     theaters: res.data.subjects,
+      //     title: res.data.title,
+      //     total: res.data.total
+      //   })
+    })
+  },
+  handleformSubmit(e: any) {
+    console.log(e)
   },
   onLoad() {
-    this.getIndexData()
+    this.getMovieList()
   }
 })
